0d04a05400511ecb7b0cb9a0f800c3b0f18ed80f
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-04-27 23:31:37 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const faqs = [
+    {
+      title: "Where are these chairs assembled?",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    },
+    {
+      title: "How long do I have to return my chair?",
+      text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+    },
+    {
+      title: "Do you ship to countries outside the EU?",
+      text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((faq, i) => (
+        <AccordionItem
+          title={faq.title}
+          key={faq.title}
+          num={i}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        >
+          {faq.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, children, curOpen, onOpen }) {
+  const isOpen = curOpen === num;
+
+  function handleToggle() {
+    onOpen(curOpen === num ? null : num);
+    // setIsOpen(!isOpen);
+  }
+
+  return (
+    <div
+      className={`item ${isOpen && "open"}`}
+      onClick={() => handleToggle(num)}
+    >
+      <span className="number">{num < 9 ? `0${num + 1}` : num + 1}</span>
+      <h3 className="title">{title}</h3>
+      <span className="icon">{isOpen ? "-" : "+"}</span>
+      {isOpen && (
+        <div className="content-box">
+          <p>{children}</p>
+        </div>
+      )}
+    </div>
+  );
+}
